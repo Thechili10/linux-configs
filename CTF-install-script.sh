a39648a83569089f25b71c487dbed869dced2097
Thechili10 <124422148+Thechili10@users.noreply.github.com>
2026-08-27 20:30:50 +0000

    initial-script

---
+++ CTF-install-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install -y \
+    wget \
+    curl \
+    nmap \
+    sqlmap \
+    wireshark \
+    tcpdump \
+    python3 \
+    netcat-traditional \
+    hashcat \
+    aircrack-ng \
+    audacity \
+    steghide \
+    net-tools \
+    dnsutils \
+    iproute2 \
+    traceroute \
+    whois \
+    iputils-ping \
+    nikto \
+    ffuf \
+    whatweb \
+    john \
+    hasid \
+    seclists \
+    binwalk \
+    foremost \
+    file \
+    exiftool \
+    xxd \
+    imagemagick \
+    pyhton3-pip \
+    python3-venv \
+    git \
+    build-essential \
+    tar \
+    ark \
+    nano \
+    btop \
+    fastfetch \
