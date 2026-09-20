2a5570bc84b0d205b03a133044a2791e229e3e3f
Thechili10 <124422148+Thechili10@users.noreply.github.com>
2026-09-20 03:01:51 +0000

    cwc

--- config-files/postInstall.sh
+++ config-files/postInstall.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Add Flathub Repository
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpak Desktop Applications
-flatpak install -y flathub com.brave.Browser || true
-flatpak install -y flathub com.vscodium.codium || true
-flatpak install -y flathub org.onlyoffice.desktopeditors || true
-flatpak install -y flathub com.spotify.Client || true
-flatpak install -y flathub md.obsidian.Obsidian || true
-flatpak install -y flathub net.mullvad.MullvadBrowser || true
-flatpak install -y flathub com.discordapp.Discord || true
-flatpak install -y flathub io.gitlab.librewolf-community || true
-flatpak install -y flathub org.sweethome3d.Sweethome3d || true
-flatpak install -y flathub org.localsend.localsend_app || true
-sudo snap install sqlmap
+sudo flatpak install -y flathub com.brave.Browser || true
+sudo flatpak install -y flathub com.vscodium.codium || true
+sudo flatpak install -y flathub org.onlyoffice.desktopeditors || true
+sudo flatpak install -y flathub com.spotify.Client || true
+sudo flatpak install -y flathub md.obsidian.Obsidian || true
+sudo flatpak install -y flathub net.mullvad.MullvadBrowser || true
+sudo flatpak install -y flathub com.discordapp.Discord || true
+sudo flatpak install -y flathub io.gitlab.librewolf-community || true
+sudo flatpak install -y flathub org.sweethome3d.Sweethome3d || true
+sudo flatpak install -y flathub org.localsend.localsend_app || true
+# sudo snap install sqlmap
